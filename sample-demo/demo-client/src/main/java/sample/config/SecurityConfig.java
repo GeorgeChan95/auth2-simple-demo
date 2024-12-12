56f1cb02f6e36dde3c3bc8ae08af546f9b8f3909
George <george_95@126.com>
2024-12-12 15:50:22 +0800

    更新注释

--- sample-demo/demo-client/src/main/java/sample/config/SecurityConfig.java
+++ sample-demo/demo-client/src/main/java/sample/config/SecurityConfig.java
@@ -19,34 +19,53 @@ import static org.springframework.security.config.Customizer.withDefaults;
  * @author Steve Riesenberg
  * @since 0.0.1
  */
-@EnableWebSecurity
-@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity // 启用 Spring Security 的 Web 安全特性，允许我们通过 SecurityFilterChain 自定义安全策略。
+@Configuration(proxyBeanMethods = false) // proxyBeanMethods = false 表示 Spring 不会对该配置类创建代理，提高性能。
 public class SecurityConfig {
 
+	/**
+	 * 配置 Spring Security 忽略某些静态资源的安全过滤。
+	 * @return
+	 */
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
 		return (web) -> web.ignoring().requestMatchers("/webjars/**", "/assets/**");
 	}
 
 	// @formatter:off
+
+	/**
+	 * Spring Security 的核心配置方法，用于定义 HTTP 请求的安全规则和认证方式
+	 * @param http
+	 * @param clientRegistrationRepository
+	 * @return
+	 * @throws Exception
+	 */
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http,
 			ClientRegistrationRepository clientRegistrationRepository) throws Exception {
 		http
 			.authorizeHttpRequests(authorize ->
 				authorize
-					.requestMatchers("/logged-out").permitAll()
-					.anyRequest().authenticated()
+					.requestMatchers("/logged-out").permitAll() // 对该路径的请求不需要认证，任何人都可以访问。
+					.anyRequest().authenticated() // 其他请求需要认证（用户必须登录后才能访问）。
 			)
-			.csrf(AbstractHttpConfigurer::disable)
-			.oauth2Login(withDefaults())
-			.oauth2Client(withDefaults())
-			.logout(logout ->
+			.csrf(AbstractHttpConfigurer::disable) // 禁用跨站请求伪造 (CSRF) 防护,适用于 RESTful APIs 或不需要表单提交的场景
+			.oauth2Login(withDefaults()) // 启用 OAuth2 登录功能, Spring Security 会根据配置的 ClientRegistration 自动生成登录页面，并处理 OAuth2 登录流程
+			.oauth2Client(withDefaults()) // 启用 OAuth2 客户端支持，用于发起对外部服务的 OAuth2 认证请求。
+			.logout(logout -> // 配置登出成功后的行为，使用了自定义的 oidcLogoutSuccessHandler() 方法
 				logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)));
 		return http.build();
 	}
 	// @formatter:on
 
+	/**
+	 * 核心类: OidcClientInitiatedLogoutSuccessHandler
+	 * - 专门处理 OpenID Connect (OIDC) 的登出逻辑。
+	 * - 允许应用通知 OpenID Provider (OP) 执行登出操作，并在登出后跳转到指定的页面。
+	 * @param clientRegistrationRepository
+	 * @return
+	 */
 	private LogoutSuccessHandler oidcLogoutSuccessHandler(
 			ClientRegistrationRepository clientRegistrationRepository) {
 		OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
--- sample-demo/demo-client/src/main/java/sample/config/WebClientConfig.java
+++ sample-demo/demo-client/src/main/java/sample/config/WebClientConfig.java
@@ -36,8 +36,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+	/**
+	 * 配置一个 WebClient,用于与外部服务交互。
+	 * WebClient 是 Spring 提供的非阻塞、响应式 HTTP 客户端,
+	 * 每次发送请求时，WebClient 会通过 OAuth2AuthorizedClientManager 自动处理获取或刷新 Access Token，并附加到请求头中。
+	 * @param authorizedClientManager
+	 * @return
+	 */
 	@Bean
 	public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+		// 使用 ServletOAuth2AuthorizedClientExchangeFilterFunction，它会拦截 HTTP 请求并自动为其附加 OAuth2 授权头（如 Access Token）
 		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
 				new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
 		// @formatter:off
@@ -47,26 +55,37 @@ public class WebClientConfig {
 		// @formatter:on
 	}
 
+	/**
+	 * Spring Security 提供的核心接口，管理 OAuth2 授权客户端的生命周期。
+	 * 它根据不同的授权模式和客户端配置，自动处理授权、令牌刷新等逻辑。
+	 * @param clientRegistrationRepository 用于存储和检索已注册的 OAuth2 客户端信息（ClientRegistration）
+	 * @param authorizedClientRepository 用于存储已授权的 OAuth2 客户端及其关联的访问令牌（OAuth2AuthorizedClient）。
+	 * @return
+	 */
 	@Bean
 	public OAuth2AuthorizedClientManager authorizedClientManager(
 			ClientRegistrationRepository clientRegistrationRepository,
 			OAuth2AuthorizedClientRepository authorizedClientRepository) {
 
 		// @formatter:off
+		// 配置授权提供器 (OAuth2AuthorizedClientProvider)，用于组合多个授权提供器。
 		OAuth2AuthorizedClientProvider authorizedClientProvider =
 				OAuth2AuthorizedClientProviderBuilder.builder()
-						.authorizationCode()
-						.refreshToken()
-						.clientCredentials()
-						.provider(new DeviceCodeOAuth2AuthorizedClientProvider())
+						.authorizationCode() // 支持授权码模式
+						.refreshToken() // 支持刷新令牌
+						.clientCredentials() // 支持客户端凭据模式
+						.provider(new DeviceCodeOAuth2AuthorizedClientProvider()) // 支持设备码模式（自定义）。
 						.build();
 		// @formatter:on
 
+		// DefaultOAuth2AuthorizedClientManager 将基于这两个存储库来管理 OAuth2 客户端的授权和访问令牌。
 		DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
 				clientRegistrationRepository, authorizedClientRepository);
+		// 配置 DefaultOAuth2AuthorizedClientManager 使用的 授权提供器（OAuth2AuthorizedClientProvider），以确定如何处理授权。
 		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
 
 		// Set a contextAttributesMapper to obtain device_code from the request
+		// 设置 ContextAttributesMapper， 用于从上下文中获取必要的属性（如设备码）
 		authorizedClientManager.setContextAttributesMapper(DeviceCodeOAuth2AuthorizedClientProvider
 				.deviceCodeContextAttributesMapper());
 
